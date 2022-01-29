232aa63fd3bc35b5e8e45d633282a325bf71c6e5
Danish Irfan Shah <64722277+DanishStampy@users.noreply.github.com>
2022-01-30 00:57:52 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require('./data/dummy');
+
+const app = express();
+dotenv.config();
+
+
+app.get('/', (req, res) => {
+  res.send("API is running sucessfulyy")
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+})
+
+app.get('/api/chat/:id', (req, res) => {
+  //console.log(req.params.id)
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+
+// Listen PORT
+const PORT = process.env.PORT || 5000
+
+app.listen(`${PORT}`, console.log(`Server started on ${PORT}`))
